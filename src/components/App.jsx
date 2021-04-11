fcfd58965378702dd1917ea21a4a1a2a5e259b8c
Mia Bueno <mia.bueno101@gmail.com>
2021-04-11 22:11:11 +1000

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./Header";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+const createEntry = (emoji) => {
+  return (
+    <Entry name={emoji.name} emoji={emoji.emoji} meaning={emoji.meaning} />
+  );
+};
+
+function App() {
+  return (
+    <div>
+      <Header title="emojipedia" />
+      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
+    </div>
+  );
+}
+
+export default App;
